5e6ca97d224292be25850e708738e756c34f3e28
iBreakerGTHB <ibreakergaming@gmail.com>
2025-06-12 19:46:37 +1200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Pulsars"
-  optimizeDeps: {
+  base: "/Pulsars", // make sure this matches your deploy subdirectory
+  optimizeDeps: { 
     exclude: ['lucide-react'],
   },
 });
